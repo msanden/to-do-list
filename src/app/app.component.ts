02ad767f2f8935436d41ec0d4d5530230baa3854
Manuel Sande <mnlsande22@gmail.com>
2019-03-21 09:38:23 -0400

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'to-do-list';
+  currentTask: string = 'Angular homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedTask = null;
+
+  masterTaskList: Task[] = [
+    new Task("Finish weekend Angular homework.", 3),
+    new Task('Begin brainstorming possible JavaScript group projects.', 2),
+    new Task('Add README file to last few Angular repos on GitHub.', 2)
+  ];
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+   this.selectedTask = null;
+ }
+
+  addTask(newTask: Task) {
+   this.masterTaskList.push(newTask);
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CompletenessPipe } from './completeness.pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+    CompletenessPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
